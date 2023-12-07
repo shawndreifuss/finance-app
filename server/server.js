93c1b482e19135caf580177071a3c0c2bdd0a68c
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-12-06 18:02:21 -0800

    added server

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+app.use(routes);
 // Middleware 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+
 
 
 db.once('open', () => {
