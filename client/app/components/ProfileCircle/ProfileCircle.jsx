1969b7f3f0942fb99c597980d0f13384724f7cf2
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-12-06 16:03:54 -0800

    error fixed

---
+++ client/app/components/ProfileCircle/ProfileCircle.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ProfileCircle.css'
+
+const ProfileCirlce = () => {
+  return (
+    <div className="sidebarTop">
+    <img className='profilePicture' src="/profile6.png" alt="" />
+    </div>
+  )
+}
+
+export default ProfileCirlce
---
+++ client/app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './partials/Header'
+import SideNav from './partials/SideNav'
+
+export default function Home() {
+  return (
+    <>
+   <SideNav />  
+  <Header />
+  </>
+  )
+}
